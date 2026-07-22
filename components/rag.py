438e74ef87ecfeef9c1e91aa4ea7f07078edcb39
visheshbpatel <iamvisheshpatel@gmail.com>
2026-07-22 11:28:36 +0530

    feat: create persistent Chroma vector store

--- components/rag.py
+++ components/rag.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
 
 def load_documents():
@@ -43,3 +45,17 @@ text_splitter = RecursiveCharacterTextSplitter(
 def split_documents(documents):
     return text_splitter.split_documents(documents)
 
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+
+def create_vector_store(chunks):
+
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory="data/chroma"
+    )
+
+    return vector_store
